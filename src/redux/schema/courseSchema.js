4ace83d5747a00d45d205e67fc1a35d5e401059e
ankerx <93661548+ankerx@users.noreply.github.com>
2022-09-23 12:02:25 -0400

    fix: single course page

--- src/redux/schema/courseSchema.js
+++ src/redux/schema/courseSchema.js
@@ -1,6 +1,8 @@
 export const courseSchema = {
   descriptionShort: "description_short",
+  descriptionFull: "description_full",
   thumbnailImgUrl: "thumbnail_img_url",
+  fullImgUrl: "full_img_url",
   name: "name",
   price: "price",
   level: "level",
